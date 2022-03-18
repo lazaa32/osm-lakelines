0f8b160d4e5795334f8c475359e7c225d65df5ce
acalcutt <acalcutt@techidiots.net>
2022-03-18 14:13:14 -0400

    Add a lake pre-simplification step using ST_SimplifyVW
    A rough attempt to try and get the point count down before sending it though label_centerlines. This speeds it up quote a bit (24hrs vs ~4-5days)

--- create_centerlines.sh
+++ create_centerlines.sh
@@ -9,6 +9,7 @@ CENTERLINES_GEOJSON=$EXPORT_DIR/lake_centerline.geojson
 CENTERLINES_GPKG=$EXPORT_DIR/lake_centerline.gpkg
 MAPPING_YAML=$CONFIG_DIR/lake_centerlines.yaml
 IMPOSM3_CACHE_DIR=$EXPORT_DIR/lake_centerlines_cache
+SIMPLIFY_TABLE="osm_lake_polygon_simplify"
 
 if [ ! -d "$EXPORT_DIR" ]; then mkdir -p $EXPORT_DIR; fi
 if [ ! -d "$IMPOSM3_CACHE_DIR" ]; then mkdir -p $IMPOSM3_CACHE_DIR; fi
@@ -24,15 +25,76 @@ if [ ! -f $CENTERLINES_GEOJSON ]; then
 		echo "====> : Importing plantet PBF into postgresql using imposm"
 		PG_CONNECT="postgis://$POSTGRES_USER:$POSTGRES_PASS@$POSTGRES_HOST/$POSTGRES_DB"
 		DB_SCHEMA="public"
-
+		
 		tools/imposm3/bin/imposm import -connection "$PG_CONNECT" -mapping "$MAPPING_YAML" -overwritecache -cachedir "$IMPOSM3_CACHE_DIR" -read "$FULL_PBF" -dbschema-import="$DB_SCHEMA" -write
+		echo "====> : Simplifying polygons with a lot of points"
+PGPASSWORD=$POSTGRES_PASS psql -h $POSTGRES_HOST --username="$POSTGRES_USER" <<EOSQL
+	SELECT pg_terminate_backend(pg_stat_activity.pid)
+	FROM pg_stat_activity
+	WHERE pg_stat_activity.datname = '$POSTGRES_DB'
+	  AND pid <> pg_backend_pid();
+	  
+	\c $POSTGRES_DB;
+	DROP TABLE IF EXISTS $SIMPLIFY_TABLE;
+	SELECT
+		osm_id, ST_SimplifyVW(geometry,50) As geometry, ST_Npoints(ST_SimplifyVW(geometry,100)) As points 
+	INTO $SIMPLIFY_TABLE
+	FROM
+		osm_lake_polygon
+	WHERE area > 2 * 1000 * 1000 AND ST_GeometryType(geometry)IN ('ST_Polygon','ST_MultiPolygon') AND name <> '' ORDER BY area DESC;
+		
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry,100), points = ST_Npoints(ST_SimplifyVW(geometry, 200))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry,400), points = ST_Npoints(ST_SimplifyVW(geometry, 400))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 800), points = ST_Npoints(ST_SimplifyVW(geometry, 800))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 3200), points = ST_Npoints(ST_SimplifyVW(geometry, 3200))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 6400), points = ST_Npoints(ST_SimplifyVW(geometry, 6400))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 12800), points = ST_Npoints(ST_SimplifyVW(geometry, 12800))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 25600), points = ST_Npoints(ST_SimplifyVW(geometry, 25600))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 51200), points = ST_Npoints(ST_SimplifyVW(geometry, 51200))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 102400), points = ST_Npoints(ST_SimplifyVW(geometry, 102400))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 204800), points = ST_Npoints(ST_SimplifyVW(geometry, 204800))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 409600), points = ST_Npoints(ST_SimplifyVW(geometry, 409600))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 819200), points = ST_Npoints(ST_SimplifyVW(geometry, 819200))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 1638400), points = ST_Npoints(ST_SimplifyVW(geometry, 1638400))
+	WHERE points > 3000;
+	UPDATE $SIMPLIFY_TABLE
+	SET geometry = ST_SimplifyVW(geometry, 3276800), points = ST_Npoints(ST_SimplifyVW(geometry, 3276800))
+	WHERE points > 3000;
+EOSQL
+
+
 		echo "====> : Exporting lake shapes into a shapefile"
-		query="SELECT osm_id, ST_makeValid(ST_SimplifyVW(geometry,(ST_Npoints(geometry) / 12000) * 100000)) AS geometry FROM osm_lake_polygon WHERE area > 2 * 1000 * 1000 AND ST_GeometryType(geometry)IN ('ST_Polygon','ST_MultiPolygon') AND name <> '' ORDER BY area DESC"
+		query="SELECT osm_id, ST_makeValid(geometry) AS geometry FROM $SIMPLIFY_TABLE ORDER BY points DESC, osm_id DESC"
 		pgsql2shp -f "$LAKE_SHP" -h "$POSTGRES_HOST" -u "$POSTGRES_USER" -P "$POSTGRES_PASS" "$POSTGRES_DB" "$query"
 		echo "====> : Creating a lake_centerline.geojson file from the exported shapefile"
-		label_centerlines --verbose --max_points=12000 --simplification=0.01 --smooth=2 --max_paths=1 --output_driver GeoJSON "$LAKE_SHP" "$CENTERLINES_GEOJSON"
-		#label_centerlines --verbose --max_points=12000 --simplification=0.01 --smooth=2 --max_paths=1 --output_driver GPKG "$LAKE_SHP" "$CENTERLINES_GPKG"
-		echo "====> : Creating a lake_centerline.shp file from the exported shapefile"
+		label_centerlines --verbose --max_points=3000 --simplification=0.02 --smooth=2 --max_paths=1 --output_driver GeoJSON "$LAKE_SHP" "$CENTERLINES_GEOJSON"
+		#label_centerlines --verbose --max_points=3000 --simplification=0.02 --smooth=2 --max_paths=1 --output_driver GPKG "$LAKE_SHP" "$CENTERLINES_GPKG"
+		#echo "====> : Creating a lake_centerline.shp file from the exported shapefile"
 		ogr2ogr -f "ESRI Shapefile" "$CENTERLINES_SHP" "$CENTERLINES_GEOJSON"
 	fi
 else
